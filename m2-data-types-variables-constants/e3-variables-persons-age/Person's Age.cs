ee5c48d60c7d18927be3774c10c6d521a00e432e
forged-by-sim <young.simulationtech@gmail.com>
2026-02-14 11:12:18 -0500

    edit

---
+++ m2-data-types-variables-constants/e3-variables-persons-age/Person's Age.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercise3
+{
+    /// <summary>
+    /// Integer Data Types Exercise
+    /// </summary>
+    internal class Person's Age
+    {
+        /// <summary>
+        /// Practice declaring, assigning, and printing an integer variable in C#. 
+        /// </summary>
+        /// <param name="args">command-line args</param>
+        static void Main(string[] args)
+        {
+            // Decalre, assign, and print an integer variable representing a person's age
+            int userAge; // Example of an integer variable
+            userAge = 33; // Assign a value to the variable (you can choose any age you like)
+
+            // print the value of userAge
+            Console.WriteLine("userAge: " + userAge);
+        }
+    }
+}
