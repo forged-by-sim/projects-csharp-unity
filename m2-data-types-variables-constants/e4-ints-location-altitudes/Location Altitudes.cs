57bf48e8ce59d2994958ca7bffe4c42a708d8de5
forged-by-sim <young.simulationtech@gmail.com>
2026-02-24 10:32:14 -0500

    edit

---
+++ m2-data-types-variables-constants/e4-ints-location-altitudes/Location Altitudes.cs
@@ -0,0 +1,27 @@
+﻿namespace Exercise4
+{
+    /// <summary>
+    /// Integers Calculation
+    /// </summary>
+    internal class Location Altitudes
+    {
+        /// <summary>
+        /// Practice with integers calculation, including addition and subtraction in C#.
+        /// </summary>
+        /// <param name="args">command-line args</param>
+        static void Main(string[] args)
+        {
+            // Prompt for and get the altitude for a location as an int.
+            int altitudeAlaska = 6190; // Integer variable of Denali, Alaska in meters
+
+            // Prompt for and get the altitude for a second location as an int.
+            int altitudeSouthAmerica = 6961; // Integer variable of Aconcagua, South America in meters
+
+            // Calculate the difference in altitude between the two locations.
+            int altitudeChange = altitudeSouthAmerica - altitudeAlaska;
+
+            // Display the result in meters.
+            Console.WriteLine($"The altitude change from Alaska to South America is {altitudeChange} meters.");
+        }
+    }
+}
